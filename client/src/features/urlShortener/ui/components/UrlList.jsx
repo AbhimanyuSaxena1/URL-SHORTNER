c4f064864dd64e56637d9686970578bf580d5c3f
abhimanyusaxena1 <abhisaxena215.com>
2026-09-13 14:07:48 +0530

    changed frontend url in shorten url

--- client/src/features/urlShortener/ui/components/UrlList.jsx
+++ client/src/features/urlShortener/ui/components/UrlList.jsx
@@ -49,7 +49,7 @@ const UrlList = () => {
         <div className="space-y-3">
 
           {shortenedUrls.map((urlObj) => {
-            const shortUrl = `import.meta.env.VITE_FRONTEND_URL/${urlObj.code}`
+            const shortUrl = `${import.meta.env.VITE_FRONTEND_URL/urlObj.code}`
 
             return (
               <div
